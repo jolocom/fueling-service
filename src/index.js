1f8d527f14f416cfde3595cc7b2f4d1edde6b193
Exulansis <rusu.eugeniu@gmx.de>
2018-02-08 12:39:46 +0100

    Reintroduced lost code regarding cors.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ const config = require('../config.json')
 const app = express()
 const fueler = new fuelAgent()
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 

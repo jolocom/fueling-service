634290256db3ddc602d9d7c4211f49aafb1fd866
Exulansis <rusu.eugeniu@gmx.de>
2018-02-08 12:31:06 +0100

    Awaiting confirmation.

--- src/index.js
+++ src/index.js
@@ -10,19 +10,14 @@ const fueler = new fuelAgent()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
-
 app.get('/balance', (req, res) => {
   fueler.getBalance().then(sum => res.send(sum))
 })
 
 app.post('/request', (req, res) => {
   fueler.sendEther(req.body.address)
-    .then(res.sendStatus(200))
+    .then(() => res.sendStatus(200))
+    .catch((err) => res.status(500).send(err.toString()))
 })
 
 app.listen(config.port, () => 

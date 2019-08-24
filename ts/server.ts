de47385bf14044ec0f0579331a53ec368b28988f
Exulansis <rusu.eugeniu@gmx.de>
2019-08-24 18:06:36 +0200

    Restored server.ts, added endpoint to get all addresses + balances

--- ts/server.ts
+++ ts/server.ts
@@ -1,31 +1,38 @@
-import * as express from 'express'
-import * as bodyParser from 'body-parser'
-import { FuelService } from './fuelAgent'
-import { config } from './config'
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { FuelService } from "./fuelAgent";
+import { config } from "./config";
+import { zip } from "ramda";
 
-const FuellingService = new FuelService()
-// const app = express()
-// const fueler = new fuelService()
-//
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*")
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-//   next()
-// })
-//
-// app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(bodyParser.json())
-//
-// app.get('/balance', (req, res) => {
-//   fueler.getBalance().then(sum => res.send(sum))
-// })
-//
-// app.post('/request', (req, res) => {
-//   fueler.sendEther(req.body.address)
-//     .then(() => res.sendStatus(200))
-//     .catch((err) => res.status(500).send(err.toString()))
-// })
-//
-// app.listen(config.port, () =>
-//   console.log(`Service running on ${config.port}`)
-// )
+const FuellingService = new FuelService();
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/balance", (req, res) => {
+  FuellingService.getTotalBalance().then(sum => res.json(sum));
+});
+
+app.get("/balances", (req, res) => {
+  FuellingService.getAllBalances().then(balances =>
+    res.json(zip(balances, FuellingService.keyManager.getAllAddresses()))
+  );
+});
+
+app.post("/request", (req, res) => {
+  FuellingService.sendEther(req.body.address)
+    .then(() => res.sendStatus(200))
+    .catch(err => res.status(500).send(err.toString()));
+});
+
+app.listen(config.port, () => console.log(`Service running on ${config.port}`));

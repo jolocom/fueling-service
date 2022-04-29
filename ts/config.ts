617afc5d716830e867dc4146be99b31b1d5423b1
Exulansis <rusu.eugeniu@gmx.de>
2022-04-29 12:11:52 +0200

    Updated to EIP1559 style transaction

--- ts/config.ts
+++ ts/config.ts
@@ -1,13 +1,16 @@
 export const config = {
+  // Infura credentials
+  projectSecret: "",
+  projectId: "",
+
   port: 8000,
   blackListFile: './blackList.json',
-  endpoint: 'https://rinkeby.infura.io',
+  endpoint: 'https://rinkeby.infura.io/v3/64fa85ca0b28483ea90919a83630d5d8',
   rateLimit: {
     windowMs: 24 * 60 * 60 * 1000,
     max: 10
   },
-  seedPhrase:
-    'man area total crystal walk gate rug always eyebrow flash uncover isolate',
+  seedPhrase: '',
   gasLimit: 21000,
   gasPrice: 3e9,
   amount: 0.01,
